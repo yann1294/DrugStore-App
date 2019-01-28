648ba6ae8f8cf73c041eefd6322690eee58b8381
yann1294 <dev.yann12@gmail.com>
2019-01-29 02:55:09 +0530

    update UI with bootstrap 4

--- Module 1/UI/Employee Registration/custom.js
+++ Module 1/UI/Employee Registration/custom.js
@@ -1,35 +1,3 @@
-$(function(){
-    var stateOptions;
-    var districtOptions;
-    $.getJSON('indianStates.1.json',function(result){
-        $.each(result, function(stateCode,stateName) {
-            //<option value='stateCode'>stateName</option>
-            stateOptions+="<option value='"
-            +stateCode+
-            "'>"
-            +stateName+
-            "</option>";
-             });
-             $('#state').html(stateOptions);
-        });
-
-        $("#state").change(function(){
-            
-                    $.getJSON('MHDistricts.json',function(result){
-                    $.each(result, function(i,district) {
-                        //<option value='districtName'>districtName</option>
-                        districtOptions+="<option value='"
-                        +district.name+
-                        "'>"
-                        +district.name+
-                        "</option>";
-                         });
-                         $('#district').html(districtOptions);
-                    });
-                
-            });
-    
-});
 function isNumberKey(evt)
 {
     var charCode = (evt.which) ? evt.which : evt.keyCode;
